cad348320268a19a3c8db6d687f4a501783dbd93
ColeSexton <colesaudio88@gmail.com>
2025-07-15 09:53:26 -0500

    created front end and backend files

---
+++ U2-final-backend/src/main/java/com/example/U2_final_backend/U2FinalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.U2_final_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2FinalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2FinalBackendApplication.class, args);
+	}
+
+}
---
+++ U2-final-backend/src/test/java/com/example/U2_final_backend/U2FinalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.U2_final_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2FinalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
