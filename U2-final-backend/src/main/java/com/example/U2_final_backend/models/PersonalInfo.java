673e2144c5f5d12e56df2c1b9addf9429d44793d
ColeSexton <colesaudio88@gmail.com>
2025-07-16 09:47:14 -0500

    created PersonalInfo model class

--- U2-final-backend/src/main/java/com/example/U2_final_backend/models/PersonalInfo.java
+++ U2-final-backend/src/main/java/com/example/U2_final_backend/models/PersonalInfo.java
@@ -1,5 +1,56 @@
 package com.example.U2_final_backend.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class PersonalInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name= "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    public PersonalInfo() {
+    }
+
+    public PersonalInfo(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
 }
