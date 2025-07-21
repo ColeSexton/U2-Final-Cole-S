979c80ba41359303f751f598d81b81f04e9436f1
ColeSexton <colesaudio88@gmail.com>
2025-07-21 14:14:26 -0500

    updated SongFormSubmissionDTO

--- U2-final-backend/src/main/java/com/example/U2_final_backend/dto/SongFormSubmissionDTO.java
+++ U2-final-backend/src/main/java/com/example/U2_final_backend/dto/SongFormSubmissionDTO.java
@@ -1,4 +1,25 @@
 package com.example.U2_final_backend.dto;
 
+import com.example.U2_final_backend.models.PersonalInfo;
+import com.example.U2_final_backend.models.SongFormInfo;
+
 public class SongFormSubmissionDTO {
+    private PersonalInfo personalInfo;
+    private SongFormInfo songFormInfo;
+
+    public PersonalInfo getPersonalInfo() {
+        return personalInfo;
+    }
+    public void setPersonalInfo(PersonalInfo personalInfo) {
+        this.personalInfo = personalInfo;
+    }
+
+    public SongFormInfo getSongFormInfo() {
+        return songFormInfo;
+    }
+
+    public void setSongFormInfo(SongFormInfo songFormInfo) {
+        this.songFormInfo = songFormInfo;
+    }
+
 }
