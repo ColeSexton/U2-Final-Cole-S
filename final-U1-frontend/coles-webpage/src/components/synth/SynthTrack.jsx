86ec94ee4629f3f7ab76b7d8d2e945ed11de85f2
ColeSexton <colesaudio88@gmail.com>
2025-07-23 14:42:40 -0500

    added keyboard note map middle c to ocative above

---
+++ final-U1-frontend/coles-webpage/src/components/synth/SynthTrack.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { createSynth } from '../../utils/audioUtils';
+
+const keyboardNotes = {
+    a: 261.63, //c 
+    w: 277.18, //c#
+    s: 293.66, // d
+    e: 311.13, //d#
+    d: 329.63, //e
+    f: 349.23, //f
+    t: 369.99, //f#
+    g: 392, //g
+    y: 415.30, //g#
+    h: 440, //a
+    u: 466.16, //a#
+    j: 493.88, //b
+    k: 523.25 //c
+}
