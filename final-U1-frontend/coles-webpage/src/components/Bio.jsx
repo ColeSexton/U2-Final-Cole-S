4272d64118b940d0b1ec3d3cb23fb5f0089df357
ColeSexton <colesaudio88@gmail.com>
2025-08-04 10:27:09 -0500

    updated footer component, css, and bio page to have footer

--- final-U1-frontend/coles-webpage/src/components/Bio.jsx
+++ final-U1-frontend/coles-webpage/src/components/Bio.jsx
@@ -6,8 +6,9 @@ import { FaYoutube } from "react-icons/fa";
 const Bio = () =>{
 
     return(   
-        <div>
+        <div className="bioPageContainer">
             <Header/>
+            <div className="bioWrap">
 
             <img id="profilePic" src= "/profilePIC.jpg" alt="Cole in Colorado, in front of mountains."/>
          
@@ -19,11 +20,12 @@ const Bio = () =>{
                 I would also love to write you a song or soundscape. Whether its for a loved one, yourself,<br />
                 or a project you are working on, I am eager to help! Find the Song Form below, let me know<br />
                 the details, and you will have your own original song in no time! <br />
-                Check out the Generator page for creative ideas for when you are feeling stuck!
+                Check out the Generator page for creative ideas for when you are feeling stuck! <br/>
+                On the Synth page there is a Bass and Lead synthesizer that you can play!
             </p>
 
                 <div id="bioLinks">
-                    <p>
+                    <div>
                         <h3>Links to my Music</h3>
                         <br />
 
@@ -34,10 +36,14 @@ const Bio = () =>{
                         <a href={'https://open.spotify.com/artist/4ZsyQ1tTX0Fqz6PrUw2MsW'} target="_blank"><i className="fa-brands fa-spotify fa-2x" /></a>
 
                         <a href={'https://www.youtube.com/watch?v=LZc4Tk4U1_M&list=OLAK5uy_m34yo8HtKXGJtFsrj5ygXFrp4UA9RtRw0'} target="_blank"><i className="fa-brands fa-youtube fa-2x" /></a>
-                    </p>
+                    </div>
 
                    
                 </div>
+                
+            </div>
+
+            <Footer />
                
         </div>
     )
